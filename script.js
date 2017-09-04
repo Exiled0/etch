76d36d1fddf1497117826eb4bfcc767b586a0d89
exiled0 <0exiled@gmail.com>
2017-09-03 19:14:16 -0700

    Pieces fitting on board correctly.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var numPiece = 16;
+var pieceHeight = 400/numPiece;
+var pieceWidth = 500/numPiece;
+
 $(document).ready(function() {
-	$('#board').append("<div class='piece'></div>");
+	/*something with the divs/for loop is adding spcae for next line...
+	  Update - set board font-size to 0 */
+	for(i=1; i<=numPiece * numPiece; i++) {
+		$('#board').append("<div class='piece'></div>");
+	}
+	$(".piece").css("height", pieceHeight + "px");
+	$(".piece").css("width", pieceWidth + "px");
 });
