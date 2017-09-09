c160eb24b1b5e182b9dd644083be69e581902a24
exiled0 <0exiled@gmail.com>
2017-09-08 22:17:40 -0700

    Commit grid resize and "fix" resizing issues. Add option to make random colors.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 var numPiece = 16;
-var pieceHeight = 400/numPiece;
-var pieceWidth = 500/numPiece;
+var pieceHeight, pieceWidth;
 var etchedRGB = 50;
+var colorR;
+var colorG;
+var colorB;
 
 $(document).ready(function() {
-	/*something with the divs/for loop is adding spcae for next line...
+	/*something with the divs/for loop is adding space for next line...
 	  Update - set board font-size to 0 */
+	  reset();
+
+	  /*$('#shake').onclick="reset()";*/
+});
+
+function reset() {
+	$(".piece").remove();	//clear all pieces
+	var pieceHeight = 400/numPiece; //helps with overflow
+	var pieceWidth = 500/numPiece;	//but still not right
+		//might have to switch from inline block to another way...
+
 	for(i=1; i<=numPiece * numPiece; i++) {
 		$('#board').append("<div class='piece'></div>");
 	}
@@ -14,6 +27,28 @@ $(document).ready(function() {
 
 	/*Etching*/
 	$(".piece").mouseenter(function() {
-		$(this).css("background-color", "rgb(" + etchedRGB + ", " + etchedRGB + ", " + etchedRGB + ")" );
+		var rainbowChecked = $('#rainbow').is(":checked");
+		if (rainbowChecked) {
+			colorR = Math.floor(Math.random() * 255);
+			colorG = Math.floor(Math.random() * 255);
+			colorB = Math.floor(Math.random() * 255);
+			$(this).css("background-color", "rgb(" + colorR + ", " + colorG + ", " + colorB + ")" );	
+		} else {
+			$(this).css("background-color", "rgb(" + etchedRGB + ", " + etchedRGB + ", " + etchedRGB + ")" );
+		}
 	});
-});
+}
+
+function resize() {
+	var promptSize = prompt("How many pieces should be on a side? (Enter an Integer between 1 and 64)", "16");
+	var newSize = parseInt(promptSize);
+	if (Number.isInteger(newSize)) {
+		if (0 < newSize && newSize <= 64) {
+			numPiece = newSize;
+			reset();
+		} else {
+			numPiece = 16;
+			reset();
+		}
+	} 
+}
